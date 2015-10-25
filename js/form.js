eec34d0c62c9b461ce494ef6b919f07be63039a5
Somushkin Maksym <nesterenkomakiss@yandex.ua>
2015-10-25 11:59:18 +0200

    Improved previewing and enabled deleting of uploaded photos

--- js/form.js
+++ js/form.js
@@ -2,13 +2,20 @@
   if (!("FormData" in window)) {
   return;
   }
-
+  
   var form = document.querySelector(".form");
+  var template = document.querySelector("#image-template").innerHTML;
+  var area = document.querySelector(".form__upload-area");
+  var queue = [];
 
   form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     var data = new FormData(form);
+
+    queue.forEach(function (element) {
+      data.append("images", element.file);
+    });
 
     request(data, function(response) {
       console.log(response);
@@ -31,35 +38,47 @@
   }
 
   if ("FileReader" in window) {
-    var area = document.querySelector(".form__upload-area");
     form.querySelector("#upload_photo").addEventListener("change", function() {
       var files = this.files;
       for (var i = 0; i < files.length; i++) {
         preview(files[i]);
       }
+      this.value = "";
     });
 
     function preview(file) {
       if (file.type.match(/image.*/)) {
         var reader = new FileReader();
         reader.addEventListener("load", function(event) {
-          var delButton = document.createElement("div");
-          delButton.classList.add("form__delete-file");
-          var img = document.createElement("img");
-          img.src = event.target.result;
-          img.alt = file.name;
-          var fileDescr = document.createElement("span");
-          fileDescr.classList.add("form__uploaded-descr");
-          fileDescr.innerText = file.name;
-          var uploadedPhoto = document.createElement("div");
-          uploadedPhoto.classList.add("form__uploaded-file");
-          uploadedPhoto.appendChild(delButton);
-          uploadedPhoto.appendChild(img);
-          uploadedPhoto.appendChild(fileDescr);
-          area.appendChild(uploadedPhoto);
+          var html = Mustache.render(template, {
+            "image": event.target.result,
+            "name": file.name
+          });
+
+          var div = document.createElement("div");
+          div.classList.add("form__uploaded-file");
+          div.innerHTML = html;
+
+          area.appendChild(div);
+
+          div.querySelector(".form__delete-file").addEventListener("click",
+          function(event) {
+            event.preventDefault();
+            removePreview(div);
+          });
+
+          queue.push({"file": file, "div": div});
         });
         reader.readAsDataURL(file);
       }
+    }
+
+    function removePreview(div) {
+      queue = queue.filter(function(element) {
+        return element.div != div;
+      });
+
+      div.parentNode.removeChild(div);
     }
   }
 })();
