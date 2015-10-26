7041e76e79eafb922ef75164d7a5a3c7a03d09c3
Somushkin Maksym <nesterenkomakiss@yandex.ua>
2015-10-26 04:07:28 +0200

    Automation of the project with Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,40 @@ module.exports = function(grunt) {
   var config = {
     pkg: grunt.file.readJSON('package.json'),
 
+    clean: {
+      build: ["build"]
+    },
+
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "source",
+          src: [
+            "img/**",
+            "js/**",
+            "index.html",
+            "form.html",
+            "blog.html",
+            "post.html"
+          ],
+          dest: "build"
+        }]
+      }
+    },
+
     sass: {
       style: {
         files: {
-          'css/style.css': 'sass/style.scss'
+          'build/css/style.css': 'source/sass/style.scss'
+        }
+      }
+    },
+
+    cmq: {
+      style: {
+        files: {
+          'build/css/style.css': 'build/css/style.css'
         }
       }
     },
@@ -21,18 +51,67 @@ module.exports = function(grunt) {
         ]
       },
       style: {
-        src: 'css/*.css'
+        src: 'build/css/*.css'
+      }
+    },
+
+    cssmin: {
+      options: {
+        keepSpecialComments: 0,
+        report: 'gzip'
+      },
+      style: {
+        files: {
+          'build/css/style.min.css': 'build/css/style.css'
+        }
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png,jpg,gif,svg}"]
+        }]
+      }
+    },
+
+    htmlmin: {
+      options: {
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        caseSensitive: true,
+        keepClosingSlash: false
+      },
+      html: {
+        files: {
+          "build/index.min.html": "build/index.html",
+          "build/form.min.html": "build/form.html",
+          "build/blog.min.html": "build/blog.html",
+          "build/post.min.html": "build/post.html"
+        }
       }
     },
 
     watch: {
       style: {
-        files: ['sass/**/*.scss'],
-        tasks: ['sass', 'postcss'],
+        files: ['source/sass/**/*.scss'],
+        tasks: ['sass', 'cmq', 'postcss'],
         options: {
           spawn: false,
           livereload: true
         }
+      }
+    },
+
+    csscomb: {
+      style: {
+      expand: true,
+      src: ["source/sass/**/*.scss"]
       }
     }
   };
@@ -40,4 +119,15 @@ module.exports = function(grunt) {
   config = require('./.gosha')(grunt, config);
 
   grunt.initConfig(config);
+
+  grunt.registerTask('build', [
+      'clean',
+      'copy',
+      'sass',
+      'cmq',
+      'postcss',
+      'cssmin',
+      'imagemin',
+      'htmlmin'
+    ]);
 };
